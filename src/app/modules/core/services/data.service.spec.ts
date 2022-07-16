2bbdc0e62c0be623a40d03b9a212fcb47f85684c
kulichkoff <kulichkov@background.digital>
2022-07-16 23:34:30 +0300

    edited tests

--- src/app/modules/core/services/data.service.spec.ts
+++ src/app/modules/core/services/data.service.spec.ts
@@ -1,16 +1,50 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DataService } from './data.service';
+import {
+    HttpClientTestingModule,
+    HttpTestingController,
+} from '@angular/common/http/testing';
+import { ICar } from '../models/car.interface';
 
 describe('DataService', () => {
     let service: DataService;
+    let httpMock: HttpTestingController;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule,
+            ],
+        });
         service = TestBed.inject(DataService);
+        httpMock = TestBed.inject(HttpTestingController);
+    });
+
+    afterEach(() => {
+        httpMock.verify();
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    describe('#findAllCars', () => {
+        it('should return an Observable<ICar[]>', () => {
+            const dummyCars = [
+                { id: 0, Name: 'chevrolet camaro' },
+                { id: 1, Name: 'dodge challenger' },
+            ];
+
+            service.findAllCars(0, 2)
+                .subscribe(cars => {
+                    expect(cars.length).toBe(2);
+                    expect(cars).toEqual(dummyCars as ICar[]);
+                });
+
+            const req = httpMock.expectOne('/api/cars?_page=1&_limit=2');
+            expect(req.request.method).toBe('GET');
+            req.flush(dummyCars);
+        });
     });
 });
